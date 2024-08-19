7d934d394887b88607f3f993896363e2d09e3605
MRINMOY MONDAL <mm2922894@gmail.com>
2024-08-20 03:42:38 +0530

    test

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,10 +1,16 @@
 import { Material3ThemeProvider } from "@/components/providers/Material3ThemeProvider";
 import { persistor, store } from "@/features/store";
 import Ionicons from "@expo/vector-icons/Ionicons";
+import * as NavigationBar from "expo-navigation-bar";
 import { Stack } from "expo-router/stack";
+import { GestureHandlerRootView } from "react-native-gesture-handler";
 import { ActivityIndicator } from "react-native-paper";
+import { ReducedMotionConfig, ReduceMotion } from "react-native-reanimated";
 import { Provider } from "react-redux";
 import { PersistGate } from "redux-persist/integration/react";
+
+NavigationBar.setPositionAsync("absolute");
+NavigationBar.setBackgroundColorAsync("#00000000");
 
 export default function Layout() {
   return (
@@ -15,15 +21,16 @@ export default function Layout() {
             icon: (props: any) => <Ionicons {...props} />,
           }}
         >
-          <Stack
-            screenOptions={{
-              animation: "default",
-              headerShown: false,
-            }}
-            initialRouteName="/"
-          >
-            <Stack.Screen name="index" options={{}} />
-          </Stack>
+          <GestureHandlerRootView style={{ flex: 1 }}>
+            <ReducedMotionConfig mode={ReduceMotion.Never} />
+            <Stack
+              screenOptions={{
+                animation: "default",
+                headerShown: false,
+              }}
+              initialRouteName="/"
+            />
+          </GestureHandlerRootView>
         </Material3ThemeProvider>
       </PersistGate>
     </Provider>
