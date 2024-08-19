eee2838b1953e9d9b30a0cd656dc7cf42f1f054d
MRINMOY MONDAL <mm2922894@gmail.com>
2024-08-19 21:50:31 +0530

    up

--- src/components/StarMap.tsx
+++ src/components/StarMap.tsx
@@ -1,5 +1,6 @@
-import { Canvas, Circle } from "@shopify/react-native-skia";
+import { Canvas, Circle, Text } from "@shopify/react-native-skia";
 import React, { useState } from "react";
+import { Dimensions } from "react-native";
 
 interface Star {
   DE: number;
@@ -14,19 +15,30 @@ interface StarMapProps {
 }
 
 export default function StarMap({ stars }: StarMapProps) {
+  const { width, height } = Dimensions.get("screen");
   const [scale, setscale] = useState<number>(1);
+
+  const projectTo2d = ({ RA, DE }: { RA: number; DE: number }) => {
+    const x = (RA / 360) * width;
+    const y = ((90 - DE) / 180) * height;
+
+    return { x, y };
+  };
 
   return (
     <Canvas className="flex-1" style={{ transform: [{ scale }] }}>
-      {stars.map((star, index) => (
-        <Circle
-          key={index}
-          color="#ffffff"
-          cx={star.DE}
-          cy={star.AM}
-          r={star.RA}
-        />
-      ))}
+      {stars.map((star, index) => {
+        const { x, y } = projectTo2d({ RA: star.RA, DE: star.DE });
+        const size = Math.max(1, 5 - star.AM / 2);
+        const color = star.t === "Oc" ? "#00f" : #ffffff;
+
+        return (
+          <>
+            <Circle cx={x} cy={y} r={size} color={color} />
+            <Text x={x + 5} y={y - 5} text={star.name} color="#fff" />
+          </>
+        );
+      })}
     </Canvas>
   );
 }
