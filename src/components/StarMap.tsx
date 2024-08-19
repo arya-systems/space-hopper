bdb722789b84e3d2156e3021337ef035b669f9f9
MRINMOY MONDAL <mm2922894@gmail.com>
2024-08-20 00:44:39 +0530

    sucessfully print3d allstars

--- src/components/StarMap.tsx
+++ src/components/StarMap.tsx
@@ -1,6 +1,6 @@
-import { Canvas, Circle, Text } from "@shopify/react-native-skia";
+import { Canvas, Circle, matchFont, Text } from "@shopify/react-native-skia";
 import React, { useState } from "react";
-import { Dimensions } from "react-native";
+import { Dimensions, Platform } from "react-native";
 
 interface Star {
   DE: number;
@@ -18,6 +18,12 @@ export default function StarMap({ stars }: StarMapProps) {
   const { width, height } = Dimensions.get("screen");
   const [scale, setscale] = useState<number>(1);
 
+  const fontFamily = Platform.select({ ios: "Helvetica", default: "serif" });
+  const fontStyle = {
+    fontFamily,
+  };
+  const font = matchFont(fontStyle);
+
   const projectTo2d = ({ RA, DE }: { RA: number; DE: number }) => {
     const x = (RA / 360) * width;
     const y = ((90 - DE) / 180) * height;
@@ -26,19 +32,28 @@ export default function StarMap({ stars }: StarMapProps) {
   };
 
   return (
-    <Canvas className="flex-1" style={{ transform: [{ scale }] }}>
+    <Canvas
+      className="flex-1 bg-black"
+      style={{ width, height, transform: [{ scale }] }}
+    >
       {stars.map((star, index) => {
         const { x, y } = projectTo2d({ RA: star.RA, DE: star.DE });
         const size = Math.max(1, 5 - star.AM / 2);
-        const color = star.t === "Oc" ? "#00f" : #ffffff;
+        const color = star.t === "Oc" ? "#00f" : "#ffffff";
 
         return (
-          <>
+          <React.Fragment key={index}>
             <Circle cx={x} cy={y} r={size} color={color} />
-            <Text x={x + 5} y={y - 5} text={star.name} color="#fff" />
-          </>
+          </React.Fragment>
         );
       })}
     </Canvas>
   );
 }
+// <Text
+//   x={x + 5}
+//   y={y - 5}
+//   text={star.name}
+//   color="#ffffff"
+//   font={font}
+// />
